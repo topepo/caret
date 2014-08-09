56817a3718253c81121ac49163084beaf2a918ce
topepo <mxkuhn@gmail.com>
2014-08-09 19:56:18 -0400

    initial version

---
+++ models/files/FRBCS.W.R
@@ -0,0 +1,39 @@
+modelInfo <- list(label = "Fuzzy Rules with Weight Factor",
+                  library = "frbs",
+                  type = "Classification",
+                  parameters = data.frame(parameter = c('num.labels', 'type.mf'),
+                                          class = c("numeric", "character"),
+                                          label = c('#Fuzzy Terms', 'Membership Function')),
+                  grid = function(x, y, len = NULL)
+                    expand.grid(num.labels = 1+(1:len)*2,      
+                                type.mf = c("GAUSSIAN", "TRAPEZOID", "TRIANGLE")),
+                  loop = NULL,
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) { 
+                    args <- list(data.train = cbind(x, as.numeric(y)),
+                                 method.type = "FRBCS.W")
+                    args$range.data <- apply(x, 2, range)
+                    
+                    theDots <- list(...)
+                    if(any(names(theDots) == "control")) {
+                      theDots$control$num.labels <- param$num.labels                  
+                      theDots$control$type.mf <- param$type.mf
+                    } else theDots$control <- list(num.labels  = param$num.labels,                  
+                                                   type.mf     = param$type.mf,
+                                                   type.tnorm = "MIN", 
+                                                   type.snorm = "MAX",
+                                                   type.implication.func = "ZADEH",
+                                                   num.class = length(unique(y)),
+                                                   name="sim-0")     
+                    do.call("frbs.learn", c(args, theDots))
+                    
+                    },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    modelFit$obsLevels[predict(modelFit, newdata)[,1]]
+                  },
+                  prob = NULL,
+                  predictors = function(x, ...){
+                    x$colnames.var[x$colnames.var %in% as.vector(x$rule)]
+                  },
+                  tags = c("Rule-Based Model"),
+                  levels = NULL,
+                  sort = function(x) x[order(x$num.labels),])
---
+++ models/files/GFS.FR.MOGAL.R
@@ -0,0 +1,42 @@
+modelInfo <- list(label = "Fuzzy Rules via MOGUL",
+                  library = "frbs",
+                  type = "Regression",
+                  parameters = data.frame(parameter = c('max.gen', 'max.iter', 'max.tune'),
+                                          class = rep("numeric", 3),
+                                          label = c('Max. Generations', 'Max. Iterations',
+                                                    'Max. Tuning Iterations')),
+                  grid = function(x, y, len = NULL)
+                    expand.grid(max.gen = 10*(1:len),      
+                                max.iter = 100,
+                                max.tune = 10*(1:len)),
+                  loop = NULL,
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) { 
+                    args <- list(data.train = cbind(x, y),
+                                 method.type = "GFS.FR.MOGUL")
+                    args$range.data <- apply(args$data.train, 2, range)
+                    
+                    theDots <- list(...)
+                    if(any(names(theDots) == "control")) {
+                      theDots$control$max.gen <- param$max.gen                  
+                      theDots$control$max.iter <- param$max.iter
+                      theDots$control$max.tune <- param$max.tune 
+                    } else theDots$control <- list(max.gen = param$max.gen,                  
+                                                   max.iter = param$max.iter,
+                                                   max.tune = param$max.tune,
+                                                   persen_cross = 0.6,
+                                                   persen_mutant = 0.3,
+                                                   epsilon = 0.4,
+                                                   name="sim-0")     
+                    do.call("frbs.learn", c(args, theDots))
+                    
+                    },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    predict(modelFit, newdata)
+                  },
+                  prob = NULL,
+                  predictors = function(x, ...){
+                    x$colnames.var[x$colnames.var %in% as.vector(x$rule)]
+                  },
+                  tags = c("Rule-Based Model"),
+                  levels = NULL,
+                  sort = function(x) x[order(x$max.iter),])
