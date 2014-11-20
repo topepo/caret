01129a10f19ce3f22fa6241ac5a73ca4121a39ef
topepo <mxkuhn@gmail.com>
2014-11-19 21:17:01 -0500

    drop = FALSE bug was fixed

--- pkg/caret/R/getTrainPerf.R
+++ pkg/caret/R/getTrainPerf.R
@@ -1,10 +1,8 @@
-getTrainPerf <- function(x)
-{
+getTrainPerf <- function(x) {
   bestPerf <- x$bestTune
   colnames(bestPerf) <- gsub("^\\.", "", colnames(bestPerf))
   out <- merge(x$results, bestPerf)
-  out <- out[, colnames(out) %in% x$perfNames]
-
+  out <- out[, colnames(out) %in% x$perfNames, drop = FALSE]
   colnames(out) <- paste("Train", colnames(out), sep = "")
   out$method <- x$method
   out
