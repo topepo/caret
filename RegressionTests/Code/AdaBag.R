f33fdad57f3354092dde781be7212c267015098b
topepo <mxkuhn@gmail.com>
2014-12-30 22:58:05 -0500

    initial versions

---
+++ RegressionTests/Code/AdaBag.R
@@ -0,0 +1,82 @@
+library(caret)
+timestamp <- format(Sys.time(), "%Y_%m_%d_%H_%M")
+
+model <- "AdaBag"
+
+#########################################################################
+
+set.seed(2)
+training <- twoClassSim(50, linearVars = 2)
+testing <- twoClassSim(500, linearVars = 2)
+trainX <- training[, -ncol(training)]
+trainY <- training$Class
+
+grid <- expand.grid(mfinal = (1:3)*3, maxdepth = c(1, 3),
+                    coeflearn = c("Breiman", "Freund", "Zhu"))
+
+cctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all",
+                       classProbs = TRUE, 
+                       summaryFunction = twoClassSummary)
+cctrl2 <- trainControl(method = "LOOCV",
+                       classProbs = TRUE, summaryFunction = twoClassSummary)
+cctrl3 <- trainControl(method = "none",
+                       classProbs = TRUE, summaryFunction = twoClassSummary)
+
+set.seed(849)
+test_class_cv_model <- train(trainX, trainY, 
+                             method = "AdaBag", 
+                             trControl = cctrl1,
+                             tuneGrid = grid,
+                             metric = "ROC", 
+                             preProc = c("center", "scale"))
+
+set.seed(849)
+test_class_cv_form <- train(Class ~ ., data = training, 
+                            method = "AdaBag",
+                            tuneGrid = grid, 
+                            trControl = cctrl1,
+                            metric = "ROC", 
+                            preProc = c("center", "scale"))
+
+test_class_pred <- predict(test_class_cv_model, testing[, -ncol(testing)])
+test_class_prob <- predict(test_class_cv_model, testing[, -ncol(testing)], type = "prob")
+test_class_pred_form <- predict(test_class_cv_form, testing[, -ncol(testing)])
+test_class_prob_form <- predict(test_class_cv_form, testing[, -ncol(testing)], type = "prob")
+
+
+set.seed(849)
+test_class_loo_model <- train(trainX, trainY, 
+                              method = "AdaBag", 
+                              trControl = cctrl2,
+                              tuneGrid = grid,
+                              metric = "ROC", 
+                              preProc = c("center", "scale"))
+
+set.seed(849)
+test_class_none_model <- train(trainX, trainY, 
+                               method = "AdaBag", 
+                               trControl = cctrl3,
+                               tuneLength = 1,
+                               metric = "ROC", 
+                               preProc = c("center", "scale"))
+
+test_class_none_pred <- predict(test_class_none_model, testing[, -ncol(testing)])
+test_class_none_prob <- predict(test_class_none_model, testing[, -ncol(testing)], type = "prob")
+
+
+test_levels <- levels(test_class_cv_model)
+if(!all(levels(trainY) %in% test_levels))
+  cat("wrong levels")
+
+#########################################################################
+
+sInfo <- sessionInfo()
+
+tests <- grep("test_", ls(), fixed = TRUE, value = TRUE)
+
+save(list = c(tests, "sInfo", "timestamp"),
+     file = file.path(getwd(), paste(model, ".RData", sep = "")))
+
+q("no")
+
+
---
+++ models/files/AdaBag.R
@@ -0,0 +1,87 @@
+modelInfo <- list(label = "Bagged AdaBoost",
+                  library = "adabag",
+                  loop = function(grid) {     
+                    loop <- ddply(grid, c("maxdepth"),
+                                  function(x) c(mfinal = max(x$mfinal)))
+                    submodels <- vector(mode = "list", length = nrow(loop))
+                    for(i in seq(along = loop$mfinal)) {
+                      index <- which(grid$maxdepth == loop$maxdepth[i])
+                      trees <- grid[index, "mfinal",drop = FALSE] 
+                      submodels[[i]] <- data.frame(mfinal = trees[trees != loop$mfinal[i]])
+                    }    
+                    list(loop = loop, submodels = submodels)
+                  },
+                  type = c("Classification"),
+                  parameters = data.frame(parameter = c('mfinal', 'maxdepth'),
+                                          class = c("numeric", "numeric"),
+                                          label = c('#Trees', 'Max Tree Depth')),
+                  grid = function(x, y, len = NULL) expand.grid(mfinal = floor((1:len) * 50),
+                                                                maxdepth = seq(1, len)),
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    theDots <- list(...)
+                    
+                    if(any(names(theDots) == "control")) {
+                      theDots$control$maxdepth <- param$maxdepth 
+                      ctl <- theDots$control
+                      theDots$control <- NULL
+                      
+                    } else ctl <- rpart.control(maxdepth = param$maxdepth,
+                                                cp=-1,minsplit=0,xval=0) 
+                    
+                    modelArgs <- c(list(formula = as.formula(.outcome ~ .),
+                                        data = if(is.data.frame(x)) x else as.data.frame(x),
+                                        mfinal = param$mfinal,            
+                                        control = ctl),
+                                   theDots)
+                    modelArgs$data$.outcome <- y
+                    out <- do.call("bagging", modelArgs)                    
+                    out     
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    if(!is.data.frame(newdata)) newdata <- as.data.frame(newdata)
+                    ## The predict function requires the outcome! Trick it by
+                    ## adding bogus data
+                    newdata$.outcome <- factor(rep(modelFit$obsLevels[1], nrow(newdata)), 
+                                               levels = modelFit$obsLevels)
+                    out <- predict(modelFit, newdata, 
+                                   newmfinal = modelFit$tuneValue$mfinal)$class
+                    
+                    if(!is.null(submodels)) {
+                      tmp <- vector(mode = "list", length = length(submodels$mfinal)+1)
+                      tmp[[1]] <- out
+                      for(i in seq(along = submodels$mfinal)) {
+                        tmp[[i+1]] <- predict(modelFit, newdata, 
+                                              newmfinal = submodels$mfinal[[i]])$class
+                      }
+                      out <- tmp
+                    }       
+                    out  
+                  },
+                  prob = function(modelFit, newdata, submodels = NULL){
+                    if(!is.data.frame(newdata)) newdata <- as.data.frame(newdata)
+                    ## The predict function requires the outcome! Trick it by
+                    ## adding bogus data
+                    newdata$.outcome <- factor(rep(modelFit$obsLevels[1], nrow(newdata)), 
+                                               levels = modelFit$obsLevels)
+                    out <- predict(modelFit, newdata)$prob
+                    colnames(out) <- modelFit$obsLevels
+                    
+                    if(!is.null(submodels)) {
+                      tmp <- vector(mode = "list", length = length(submodels$mfinal)+1)
+                      tmp[[1]] <- out
+                      for(i in seq(along = submodels$mfinal)) {
+                        tmp[[i+1]] <- predict(modelFit, newdata,  
+                                              newmfinal = submodels$mfinal[[i]])$prob
+                        colnames(tmp[[i+1]]) <- modelFit$obsLevels
+                      }
+                      out <- lapply(tmp, as.data.frame)
+                    }
+                    
+                    out 
+                  },
+                  tags = c("Tree-Based Model", "Ensemble Model", "Boosting", "Bagging",
+                           "Implicit Feature Selection"),
+                  sort = function(x) x[order(x$mfinal, x$maxdepth),])
+
+
+
