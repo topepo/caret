d621b91f08c3124a8cb61a92de910b104d404cbb
topepo <mxkuhn@gmail.com>
2015-01-28 10:56:37 -0500

    typo in expected results

--- pkg/caret/tests/testthat/trim_bayesglm.R
+++ pkg/caret/tests/testthat/trim_bayesglm.R
@@ -28,7 +28,7 @@ test_that('bayesglm classification', {
   expect_equal(predict(class_trim,   te_dat, type = "prob"),
                predict(class_notrim, te_dat, type = "prob"))
 
-  expect_less_than(object.size(class_trim)-object.size(class_notrim), 10)
+  expect_less_than(object.size(class_trim)-object.size(class_notrim), 0)
 })
 
 test_that('bayesglm regression', {
@@ -52,6 +52,6 @@ test_that('bayesglm regression', {
                                                trim = FALSE))
   expect_equal(predict(reg_trim,   te_dat),
                predict(reg_notrim, te_dat))
-  expect_less_than(object.size(reg_trim)-object.size(reg_notrim), 10)
+  expect_less_than(object.size(reg_trim)-object.size(reg_notrim), 0)
 })
 
