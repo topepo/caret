017c7cb75f9224a012c9cdaf819d0e11f0af4ec9
topepo <mxkuhn@gmail.com>
2015-04-28 10:56:06 -0400

    new model code and regression tests

---
+++ models/files/xgbLinear.R
@@ -0,0 +1,94 @@
+modelInfo <- list(label = "eXtreme Gradient Boosting",
+                  library = c("xgboost"),
+                  type = c("Regression", "Classification"),
+                  parameters = data.frame(parameter = c('nrounds', 'lambda', 'alpha'),
+                                          class = rep("numeric", 3),
+                                          label = c('# Boosting Iterations', 'L2 Regularization', 
+                                                    'L2 Regularization')),
+                  grid = function(x, y, len = NULL) 
+                    expand.grid(lambda = c(0, 10 ^ seq(-1, -4, length = len - 1)),
+                                alpha = c(0, 10 ^ seq(-1, -4, length = len - 1))),
+                  loop = NULL,
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) { 
+                    if(is.factor(y)) {
+                      if(length(lev) == 2) {
+                        y <- ifelse(y == lev[1], 1, 0) 
+                        dat <- xgb.DMatrix(as.matrix(x), label = y)
+                        out <- xgb.train(list(lambda = param$lambda, 
+                                              alpha = param$alpha), 
+                                         data = dat,
+                                         nrounds = param$nrounds,
+                                         objective = "binary:logistic",
+                                         ...)
+                      } else {
+                        y <- as.numeric(y) - 1
+                        dat <- xgb.DMatrix(as.matrix(x), label = y)
+                        out <- xgb.train(list(lambda = param$lambda, 
+                                              alpha = param$alpha), 
+                                         data = dat,
+                                         num_class = length(lev),
+                                         nrounds = param$nrounds,
+                                         objective = "multi:softprob",
+                                         ...)
+                      }     
+                    } else {
+                      dat <- xgb.DMatrix(as.matrix(x), label = y)
+                      out <- xgb.train(list(lambda = param$lambda, 
+                                            alpha = param$alpha), 
+                                       data = dat,
+                                       nrounds = param$nrounds,
+                                       objective = "reg:linear",
+                                       ...)
+                    }
+                    out
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    newdata <- xgb.DMatrix(as.matrix(newdata))
+                    out <- predict(modelFit, newdata)
+                    if(modelFit$problemType == "Classification") {
+                      if(length(modelFit$obsLevels) == 2) {
+                        out <- ifelse(out >= .5, 
+                                      modelFit$obsLevels[1], 
+                                      modelFit$obsLevels[2])
+                      } else {
+                        out <- matrix(out, ncol = length(modelFit$obsLevels), byrow = TRUE)
+                        out <- modelFit$obsLevels[apply(out, 1, which.max)]
+                      }
+                    }
+                    out  
+                  },
+                  prob = function(modelFit, newdata, submodels = NULL) {
+                    newdata <- xgb.DMatrix(as.matrix(newdata))
+                    out <- predict(modelFit, newdata)
+                    if(length(modelFit$obsLevels) == 2) {
+                      out <- cbind(out, 1 - out)
+                      colnames(out) <- modelFit$obsLevels
+                    } else {
+                      out <- matrix(out, ncol = length(modelFit$obsLevels), byrow = TRUE)
+                      colnames(out) <- modelFit$obsLevels
+                    }
+                    as.data.frame(out)
+                  },
+                  predictors = function(x, ...) {
+                    imp <- xgb.importance(x$xNames, model = x)
+                    x$xNames[x$xNames %in% imp$Feature]
+                  },
+                  varImp = function(object, numTrees = NULL, ...) {
+                    imp <- xgb.importance(object$xNames, model = object)
+                    imp <- as.data.frame(imp)[, 1:2]
+                    rownames(imp) <- as.character(imp[,1])
+                    imp <- imp[,2,drop = FALSE]
+                    colnames(imp) <- "Overall"
+                    imp   
+                  },
+                  levels = function(x) x$obsLevels,
+                  tags = c("Linear Classifier Models", 
+                           "Linear Regression Models",
+                           "L1 Regularization Models",
+                           "L2 Regularization Models",
+                           "Boosting", "Ensemble Model", "Implicit Feature Selection"),
+                  sort = function(x) {
+                    # This is a toss-up, but the # trees probably adds
+                    # complexity faster than number of splits
+                    x[order(x$nrounds, x$alpha, x$lambda),] 
+                  })
---
+++ models/files/xgbTree.R
@@ -0,0 +1,142 @@
+modelInfo <- list(label = "eXtreme Gradient Boosting",
+                  library = c("xgboost", "plyr"),
+                  type = c("Regression", "Classification"),
+                  parameters = data.frame(parameter = c('nrounds', 'max_depth', 'eta'),
+                                          class = rep("numeric", 3),
+                                          label = c('# Boosting Iterations', 'Max Tree Depth', 
+                                                    'Shrinkage')),
+                  grid = function(x, y, len = NULL) expand.grid(max_depth = seq(1, len),
+                                                                nrounds = floor((1:len) * 50),
+                                                                eta = .3),
+                  loop = function(grid) {     
+                    loop <- ddply(grid, c("eta", "max_depth"),
+                                  function(x) c(nrounds = max(x$nrounds)))
+                    submodels <- vector(mode = "list", length = nrow(loop))
+                    for(i in seq(along = loop$nrounds)) {
+                      index <- which(grid$max_depth == loop$max_depth[i] & 
+                                       grid$eta == loop$eta[i])
+                      trees <- grid[index, "nrounds"] 
+                      submodels[[i]] <- data.frame(nrounds = trees[trees != loop$nrounds[i]])
+                    }    
+                    list(loop = loop, submodels = submodels)
+                  },
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) { 
+                    if(is.factor(y)) {
+                      if(length(lev) == 2) {
+                        y <- ifelse(y == lev[1], 1, 0) 
+                        dat <- xgb.DMatrix(as.matrix(x), label = y)
+                        out <- xgb.train(list(eta = param$eta, 
+                                              max_depth = param$max_depth), 
+                                         data = dat,
+                                         nrounds = param$nrounds,
+                                         objective = "binary:logistic",
+                                         ...)
+                      } else {
+                        y <- as.numeric(y) - 1
+                        dat <- xgb.DMatrix(as.matrix(x), label = y)
+                        out <- xgb.train(list(eta = param$eta, 
+                                              max_depth = param$max_depth), 
+                                         data = dat,
+                                         num_class = length(lev),
+                                         nrounds = param$nrounds,
+                                         objective = "multi:softprob",
+                                         ...)
+                      }     
+                    } else {
+                      dat <- xgb.DMatrix(as.matrix(x), label = y)
+                      out <- xgb.train(list(eta = param$eta, 
+                                            max_depth = param$max_depth), 
+                                       data = dat,
+                                       nrounds = param$nrounds,
+                                       objective = "reg:linear",
+                                       ...)
+                    }
+                    out
+                    
+                    
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    newdata <- xgb.DMatrix(as.matrix(newdata))
+                    out <- predict(modelFit, newdata)
+                    if(modelFit$problemType == "Classification") {
+                      if(length(modelFit$obsLevels) == 2) {
+                        out <- ifelse(out >= .5, 
+                                      modelFit$obsLevels[1], 
+                                      modelFit$obsLevels[2])
+                      } else {
+                        out <- matrix(out, ncol = length(modelFit$obsLevels), byrow = TRUE)
+                        out <- modelFit$obsLevels[apply(out, 1, which.max)]
+                      }
+                    }
+                    
+                    if(!is.null(submodels)) {
+                      tmp <- vector(mode = "list", length = nrow(submodels) + 1)
+                      tmp[[1]] <- out
+                      for(j in seq(along = submodels$nrounds)) {
+                        tmp_pred <- predict(modelFit, newdata, ntreelimit = submodels$nrounds[j])
+                        if(modelFit$problemType == "Classification") {
+                          if(length(modelFit$obsLevels) == 2) {
+                            tmp_pred <- ifelse(tmp_pred >= .5, 
+                                               modelFit$obsLevels[1], 
+                                               modelFit$obsLevels[2])
+                          } else {
+                            tmp_pred <- matrix(tmp_pred, ncol = length(modelFit$obsLevels), byrow = TRUE)
+                            tmp_pred <- modelFit$obsLevels[apply(tmp_pred, 1, which.max)]
+                          }
+                        }
+                        tmp[[j+1]]  <- tmp_pred
+                      }
+                      out <- tmp
+                    }
+                    out  
+                  },
+                  prob = function(modelFit, newdata, submodels = NULL) {
+                    newdata <- xgb.DMatrix(as.matrix(newdata))
+                    out <- predict(modelFit, newdata)
+                    if(length(modelFit$obsLevels) == 2) {
+                      out <- cbind(out, 1 - out)
+                      colnames(out) <- modelFit$obsLevels
+                    } else {
+                      out <- matrix(out, ncol = length(modelFit$obsLevels), byrow = TRUE)
+                      colnames(out) <- modelFit$obsLevels
+                    }
+                    out <- as.data.frame(out)
+                             
+                    if(!is.null(submodels)) {
+                      tmp <- vector(mode = "list", length = nrow(submodels) + 1)
+                      tmp[[1]] <- out
+                      for(j in seq(along = submodels$nrounds)) {
+                        tmp_pred <- predict(modelFit, newdata, ntreelimit = submodels$nrounds[j])
+                        if(length(modelFit$obsLevels) == 2) {
+                          tmp_pred <- cbind(tmp_pred, 1 - tmp_pred)
+                          colnames(tmp_pred) <- modelFit$obsLevels
+                        } else {
+                          tmp_pred <- matrix(tmp_pred, ncol = length(modelFit$obsLevels), byrow = TRUE)
+                          colnames(tmp_pred) <- modelFit$obsLevels
+                        }
+                        tmp_pred <- as.data.frame(tmp_pred)
+                        tmp[[j+1]]  <- tmp_pred
+                      }
+                      out <- tmp
+                    }
+                    out  
+                  },
+                  predictors = function(x, ...) {
+                    imp <- xgb.importance(x$xNames, model = x)
+                    x$xNames[x$xNames %in% imp$Feature]
+                  },
+                  varImp = function(object, numTrees = NULL, ...) {
+                    imp <- xgb.importance(object$xNames, model = object)
+                    imp <- as.data.frame(imp)[, 1:2]
+                    rownames(imp) <- as.character(imp[,1])
+                    imp <- imp[,2,drop = FALSE]
+                    colnames(imp) <- "Overall"
+                    imp   
+                  },
+                  levels = function(x) x$obsLevels,
+                  tags = c("Tree-Based Model", "Boosting", "Ensemble Model", "Implicit Feature Selection"),
+                  sort = function(x) {
+                    # This is a toss-up, but the # trees probably adds
+                    # complexity faster than number of splits
+                    x[order(x$nrounds, x$max_depth, x$eta),] 
+                  })
