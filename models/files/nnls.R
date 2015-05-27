3bc5d47455d580f9ed2a275cd16b0598d61d2abc
topepo <mxkuhn@gmail.com>
2015-05-27 13:52:25 -0400

    added new method for nnls

---
+++ models/files/nnls.R
@@ -0,0 +1,29 @@
+modelInfo <- list(label = "Non-Negative Least Squares",
+                  library = "nnls",
+                  loop = NULL,
+                  type = "Regression",
+                  parameters = data.frame(parameter = "parameter",
+                                          class = "character",
+                                          label = "parameter"),
+                  grid = function(x, y, len = NULL) data.frame(parameter = "none"),
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    if(!is.matrix(x)) x <- as.matrix(x)
+                    out <- nnls(x, y)
+                    names(out$x) <- colnames(x)
+                    out
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    if(!is.matrix(newdata)) x <- as.matrix(newdata)
+                    out <- newdata %*% modelFit$x
+                    out[,1]
+                    },
+                  prob = NULL,
+                  predictors = function(x, ...) names(x$x)[x$x != 0],
+                  tags = "Linear Regression",
+                  varImp = function(object, ...) {
+                    out <- data.frame(Overall = object$x)
+                    rownames(out) <- names(object$x)
+                    out
+                    out   
+                  },
+                  sort = function(x) x)
