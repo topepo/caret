483f9aa2590635c3b9d340ef578ec21bff224070
Edwin Thoen <ethoen@jibes.nl>
2015-06-15 16:45:19 +0200

    added verbose to function

--- pkg/caret/R/findCorrelation.R
+++ pkg/caret/R/findCorrelation.R
@@ -1,15 +1,23 @@
-findCorrelation <- function(x, cutoff = .90){
+findCorrelation <- function(x, cutoff = .90, verbose = FALSE){
   averageCorr <- colMeans(abs(x))
   averageCorr <- as.numeric(as.factor(averageCorr))
   x[lower.tri(x, diag = TRUE)] <- NA
   combsAboveCutoff <- which(abs(x) > cutoff)
-  
+
   colsToCheck <- ceiling(combsAboveCutoff / nrow(x))
   rowsToCheck <- combsAboveCutoff %% nrow(x)
-  
+
   colsToDiscard <- averageCorr[colsToCheck] > averageCorr[rowsToCheck]
   rowsToDiscard <- !colsToDiscard
-  
+
+  if(verbose){
+    colsFlagged <- pmin(ifelse(colsToDiscard, colsToCheck, NA),
+                        ifelse(rowsToDiscard, rowsToCheck, NA), na.rm = TRUE)
+    cat('\n',paste('Combination row', rowsToCheck, 'and clolumn', colsToCheck,
+                   'is above the cut-off\n \t Flagging column', colsFlagged, '\n'
+                   ))
+  }
+
   deletecol <- c(colsToCheck[colsToDiscard], rowsToCheck[rowsToDiscard])
   deletecol <- unique(deletecol)
   deletecol
