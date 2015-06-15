692b9074e22be5db3ab8f15297c7191785f1eec7
Edwin Thoen <ethoen@jibes.nl>
2015-06-15 10:17:28 +0200

    vectorized implementation of findCorrelation

--- pkg/caret/R/findCorrelation.R
+++ pkg/caret/R/findCorrelation.R
@@ -1,45 +1,16 @@
-findCorrelation <- function(x, cutoff = 0.90, verbose = FALSE)
-{
-  varnum <- dim(x)[1]
+findCorrelation <- function(x, cutoff = .90){
+  averageCorr <- colMeans(abs(x))
+  averageCorr <- as.numeric(as.factor(averageCorr))
+  x[lower.tri(x, diag = TRUE)] <- NA
+  combsAboveCutoff <- which(abs(x) > cutoff)
   
-  if (!isTRUE(all.equal(x, t(x)))) stop("correlation matrix is not symmetric")
-  if (varnum == 1) stop("only one variable given")
+  colsToCheck <- ceiling(combsAboveCutoff / nrow(x))
+  rowsToCheck <- combsAboveCutoff %% nrow(x)
   
-  x <- abs(x)
+  colsToDiscard <- averageCorr[colsToCheck] > averageCorr[rowsToCheck]
+  rowsToDiscard <- !colsToDiscard
   
-  # re-ordered columns based on max absolute correlation
-  originalOrder <- 1:varnum
-  
-  averageCorr <- function(x) mean(x, na.rm = TRUE)
-  tmp <- x
-  diag(tmp) <- NA
-  
-  maxAbsCorOrder <- order(apply(tmp, 2, averageCorr), decreasing = TRUE)
-  x <- x[maxAbsCorOrder, maxAbsCorOrder]
-  newOrder <- originalOrder[maxAbsCorOrder]
-  
-  deletecol <- rep(F, varnum)
-  
-  for (i in 1:(varnum - 1)) {
-    if (deletecol[i]) next
-    for (j in (i + 1):varnum) {
-      if (!deletecol[i] & !deletecol[j]) {
-        if(verbose)
-          cat("Considering row\t", newOrder[i], 
-              "column\t", newOrder[j], 
-              "value\t", round(x[i,j], 3), "\n")  
-        if (x[i, j] > cutoff) {
-          if (mean(x[i, -i]) > mean(x[-j, j])) {
-            deletecol[i] <- T
-            if (verbose) cat("  Flagging column\t", newOrder[i], "\n")
-          }
-          else {
-            deletecol[j] <- T
-            if (verbose) cat("  Flagging column\t", newOrder[j], "\n")
-          }
-        }
-      }
-    }
-  }
-  newOrder[which(deletecol)]
+  deletecol <- c(colsToCheck[colsToDiscard], rowsToCheck[rowsToDiscard])
+  deletecol <- unique(deletecol)
+  deletecol
 }
