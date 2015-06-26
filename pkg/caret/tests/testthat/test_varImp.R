a2e51aec6c91ebf44e1b179c56ee24dff329a8ac
Homer Strong <homer.strong@gmail.com>
2015-06-26 10:31:56 -0700

    try adding library call in glmnet varImp test

--- pkg/caret/tests/testthat/test_varImp.R
+++ pkg/caret/tests/testthat/test_varImp.R
@@ -5,6 +5,7 @@ context('Testing varImp')
 test_that('glmnet varImp returns non-negative values', {
   skip_on_cran()
   set.seed(1)
+  library(glmnet)
   dat <- SLC14_1(200)
 
   reg <- train(y ~ ., data = dat,
