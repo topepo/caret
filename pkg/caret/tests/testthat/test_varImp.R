e6f7109b82bd6523654d0544821f93b2dc94c1a6
Homer Strong <homer.strong@gmail.com>
2015-06-26 12:19:30 -0700

    try loading library before skipping test

--- pkg/caret/tests/testthat/test_varImp.R
+++ pkg/caret/tests/testthat/test_varImp.R
@@ -4,8 +4,8 @@ context('Testing varImp')
 
 test_that('glmnet varImp returns non-negative values', {
   skip_on_cran()
-  skip_if_not_installed('glmnet')
   library(glmnet, verbose=TRUE)
+  skip_if_not_installed('glmnet')
   set.seed(1)
   dat <- SLC14_1(200)
 
