8a19033a6bfcd0bb48c91b3a428e4e08fcca91ef
topepo <mxkuhn@gmail.com>
2015-07-04 22:10:41 -0400

    From the rqPen package, quantile regression models rqnc and rqlasso were added.

---
+++ models/files/rqlasso.R
@@ -0,0 +1,23 @@
+modelInfo <- list(label = "Quantile Regression with LASSO penalty",
+                  library = "rqPen",
+                  type = "Regression",
+                  parameters = data.frame(parameter = 'lambda',
+                                          class = "numeric",
+                                          label = 'L1 Penalty'),
+                  grid = function(x, y, len = NULL)
+                    expand.grid(lambda = c(10 ^ seq(-1, -4, length = len))),
+                  loop = NULL,
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    rq.lasso.fit(as.matrix(x), y, lambda = param$lambda, ...)
+                    },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    predict(modelFit, newx = as.matrix(newdata))[,1]
+                  },
+                  predictors = function(x, ...) {
+                    out <- coef(x)
+                    out <- out[names(out) != "intercept"]
+                    names(out)[out != 0]
+                  },
+                  tags = c("Linear Regression", "Quantile Regression", "Implicit Feature Selection", "L1 Regularization"),
+                  prob = NULL,
+                  sort = function(x) x[order(-x$lambda),])
