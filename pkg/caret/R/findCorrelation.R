f23cbea81e69f1d7c612a46f5279e612b8c7c23a
topepo <mxkuhn@gmail.com>
2015-07-14 14:31:39 -0400

    updated with exact and approximate versions

--- pkg/caret/R/findCorrelation.R
+++ pkg/caret/R/findCorrelation.R
@@ -1,4 +1,32 @@
-findCorrelation <- function(x, cutoff = 0.90, verbose = FALSE)
+
+findCorrelation_fast <- function(x, cutoff = .90, verbose = FALSE){
+  averageCorr <- colMeans(abs(x))
+  averageCorr <- as.numeric(as.factor(averageCorr))
+  x[lower.tri(x, diag = TRUE)] <- NA
+  combsAboveCutoff <- which(abs(x) > cutoff)
+  
+  colsToCheck <- ceiling(combsAboveCutoff / nrow(x))
+  rowsToCheck <- combsAboveCutoff %% nrow(x)
+  
+  colsToDiscard <- averageCorr[colsToCheck] > averageCorr[rowsToCheck]
+  rowsToDiscard <- !colsToDiscard
+  
+  if(verbose){
+    colsFlagged <- pmin(ifelse(colsToDiscard, colsToCheck, NA),
+                        ifelse(rowsToDiscard, rowsToCheck, NA), na.rm = TRUE)
+    values <- round(x[combsAboveCutoff], 3)
+    cat('\n',paste('Combination row', rowsToCheck, 'and column', colsToCheck,
+                   'is above the cut-off, value =', values,
+                   '\n \t Flagging column', colsFlagged, '\n'
+    ))
+  }
+  
+  deletecol <- c(colsToCheck[colsToDiscard], rowsToCheck[rowsToDiscard])
+  deletecol <- unique(deletecol)
+  deletecol
+}
+
+findCorrelation_exact <- function(x, cutoff = 0.90, verbose = FALSE)
 {
   varnum <- dim(x)[1]
   
@@ -58,3 +86,18 @@ findCorrelation <- function(x, cutoff = 0.90, verbose = FALSE)
   }
   newOrder[which(deletecol)]
 }
+
+
+findCorrelation <- function(x, cutoff = 0.90, verbose = FALSE, names = FALSE, exact = ncol(x) < 100) {
+  if(names & is.null(colnames(x)))
+    stop("'x' must have column names when `names = TRUE`")
+  out <- if(exact) 
+    findCorrelation_exact(x = x, cutoff = cutoff, verbose = verbose) else 
+      findCorrelation_fast(x = x, cutoff = cutoff, verbose = verbose)
+  out
+  if(names) out <- colnames(x)[out]
+  out
+}
+
+
+
