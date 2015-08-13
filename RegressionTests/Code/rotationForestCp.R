8f598ae4a383894bc08867b6b6263fecd7cd248a
topepo <mxkuhn@gmail.com>
2015-08-13 12:18:53 -0400

    added another rotationForest method

---
+++ RegressionTests/Code/rotationForestCp.R
@@ -0,0 +1,94 @@
+library(caret)
+timestamp <- format(Sys.time(), "%Y_%m_%d_%H_%M")
+
+model <- "rotationForestCp"
+
+#########################################################################
+
+set.seed(2)
+training <- twoClassSim(50, linearVars = 2)
+testing <- twoClassSim(500, linearVars = 2)
+trainX <- training[, -ncol(training)]
+trainY <- training$Class
+
+seeds <- vector(mode = "list", length = nrow(training) + 1)
+seeds <- lapply(seeds, function(x) 1:20)
+
+cctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all",
+                       classProbs = TRUE, 
+                       summaryFunction = twoClassSummary, 
+                       seeds = seeds)
+cctrl2 <- trainControl(method = "LOOCV",
+                       classProbs = TRUE, 
+                       summaryFunction = twoClassSummary, 
+                       seeds = seeds)
+cctrl3 <- trainControl(method = "none",
+                       classProbs = TRUE, 
+                       summaryFunction = twoClassSummary,
+                       seeds = seeds)
+cctrlR <- trainControl(method = "cv", number = 3, returnResamp = "all", search = "random")
+
+set.seed(849)
+test_class_cv_model <- train(trainX, trainY, 
+                             method = "rotationForestCp", 
+                             trControl = cctrl1,
+                             metric = "ROC", 
+                             preProc = c("center", "scale"))
+
+set.seed(849)
+test_class_cv_form <- train(Class ~ ., data = training, 
+                            method = "rotationForestCp", 
+                            trControl = cctrl1,
+                            metric = "ROC", 
+                            preProc = c("center", "scale"))
+
+test_class_pred <- predict(test_class_cv_model, testing[, -ncol(testing)])
+test_class_prob <- predict(test_class_cv_model, testing[, -ncol(testing)], type = "prob")
+test_class_pred_form <- predict(test_class_cv_form, testing[, -ncol(testing)])
+test_class_prob_form <- predict(test_class_cv_form, testing[, -ncol(testing)], type = "prob")
+
+
+# currently fails from sapply misuse; maintainer notified
+# set.seed(849)
+# test_class_loo_model <- train(trainX, trainY, 
+#                               method = "rotationForestCp", 
+#                               trControl = cctrl2,
+#                               metric = "ROC", 
+#                               preProc = c("center", "scale"))
+
+set.seed(849)
+test_class_rand <- train(trainX, trainY, 
+                         method = "rotationForestCp", 
+                         trControl = cctrlR,
+                         tuneLength = 4, 
+                         preProc = c("center", "scale"))
+
+set.seed(849)
+test_class_none_model <- train(trainX, trainY, 
+                               method = "rotationForestCp", 
+                               trControl = cctrl3,
+                               tuneLength = 1,
+                               metric = "ROC", 
+                               preProc = c("center", "scale"))
+
+test_class_none_pred <- predict(test_class_none_model, testing[, -ncol(testing)])
+test_class_none_prob <- predict(test_class_none_model, testing[, -ncol(testing)], type = "prob")
+
+
+test_levels <- levels(test_class_cv_model)
+if(!all(levels(trainY) %in% test_levels))
+  cat("wrong levels")
+
+test_imp <- varImp(test_class_cv_model, scale = FALSE)
+
+#########################################################################
+
+sInfo <- sessionInfo()
+
+tests <- grep("test_", ls(), fixed = TRUE, value = TRUE)
+
+save(list = c(tests, "sInfo", "timestamp"),
+     file = file.path(getwd(), paste(model, ".RData", sep = "")))
+
+q("no")
+
---
+++ models/files/rotationForestCp.R
@@ -0,0 +1,108 @@
+modelInfo <- list(label = "Rotation Forest",
+                  library =  c("rpart", "plyr", "rotationForest"),
+                  type = c("Classification", "Regression"),
+                  parameters = data.frame(parameter = c("K", "L", 'cp'),
+                                          class = rep("numeric", 3),
+                                          label = c("#Variable Subsets", "Ensemble Size", "Complexity Parameter")),
+                  grid = function(x, y, len = NULL, search = "grid") {
+                    if(search == "grid") {
+                      out <- expand.grid(K = 1:min(len, ncol(x)-1), 
+                                         L = (1:len)*3,
+                                         cp = unique(seq(0, .1, length = len)))
+                    } else {
+                      out <- data.frame(K = sample(1:min(len, ncol(x)-1), size = len, replace = TRUE), 
+                                        L = sample(10:100, size = len, replace = TRUE),
+                                        cp = runif(len, 0, .1))
+                    }
+                    out
+                  },
+                  loop = function(grid) { 
+                    loop <- ddply(grid, .(cp, K), function(x) c(L = max(x$L)))
+                    submodels <- vector(mode = "list", length = nrow(loop))
+                    for(i in seq(along = loop$L))  {
+                      index <- which(grid$cp == loop$cp[i] & grid$K == loop$K[i])
+                      bases <- grid[index, "L"] 
+                      submodels[[i]] <- data.frame(L = bases[bases != loop$L[i]])
+                    }     
+                    list(loop = loop, submodels = submodels)
+                  },
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    if(length(lev) != 2)
+                      stop("rotationForest is only implemented for binary classification")
+                    y <- ifelse(y == lev[1], 1, 0)
+                    if(!is.data.frame(x)) x <- as.data.frame(x)
+                    theDots <- list(...)
+                    ## From ?rotationForest, the three dots only pass rpart.control
+                    ## through so we will ignore the dots after "fixing" that object
+                    if(any(names(theDots) == "control")) {
+                      theDots$control$cp <- param$cp
+                      theDots$control$xval <- 0 
+                      rpctl <- theDots$control
+                    } else rpctl <- rpart.control(cp = param$cp, xval = 0)  
+                    
+                    rotationForest(x, y, K = param$K, L = param$L, control = rpctl)
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    if(!is.data.frame(newdata)) newdata <- as.data.frame(newdata)
+                    out <- predict(modelFit, newdata)
+                    out <- ifelse(out >= .5, modelFit$obsLevels[1], modelFit$obsLevels[2])
+                    
+                    if(!is.null(submodels)) {
+                      tmp <- vector(mode = "list", length = nrow(submodels) + 1)
+                      tmp[[1]] <- out
+                      all_L <- predict(modelFit, newdata, all = TRUE)
+                      for(j in seq(along = submodels$L)) {                        
+                        tmp_pred <- apply(all_L[, 1:submodels$L[j],drop = FALSE], 1, mean)
+                        tmp[[j+1]] <- ifelse(tmp_pred >= .5, modelFit$obsLevels[1], modelFit$obsLevels[2])
+                      }
+                      out <- tmp
+                    }
+                    out   
+                  },
+                  prob = function(modelFit, newdata, submodels = NULL) {
+                    if(!is.data.frame(newdata)) newdata <- as.data.frame(newdata)
+                    all_L <- predict(modelFit, newdata, all = TRUE)
+                    out <- apply(all_L, 1, mean)
+                    out <- data.frame(x = out, y = 1 - out)
+                    colnames(out) <- modelFit$obsLevels
+                    if(!is.null(rownames(newdata))) rownames(out) <- rownames(newdata)
+                    
+                    if(!is.null(submodels)) {
+                      tmp <- vector(mode = "list", length = nrow(submodels) + 1)
+                      tmp[[1]] <- out
+                      for(j in seq(along = submodels$L)) {  
+                        tmp_pred <- apply(all_L[, 1:submodels$L[j],drop = FALSE], 1, mean)
+                        tmp_pred <- data.frame(x = tmp_pred, y = 1 - tmp_pred)
+                        colnames(tmp_pred) <- modelFit$obsLevels
+                        if(!is.null(rownames(newdata))) rownames(tmp_pred) <- rownames(newdata)
+                        tmp[[j+1]] <- tmp_pred
+                      }
+                      out <- tmp
+                    }
+                    out   
+                  },
+                  predictors = function(x, ...) {
+                    non_zero <- function(x) {                      
+                      out <- apply(x, 1, function(x) any(x != 0))
+                      names(out)[out]
+                    }
+                    sort(unique(unlist(lapply(x$loadings, non_zero))))
+                  },
+                  varImp = function(object, ...) {
+                    imps <- lapply(object$models, varImp, scale = FALSE)
+                    imps <- lapply(imps, 
+                                   function(x) {
+                                     x$Variable <- rownames(x)
+                                     x
+                                   })
+                    imps <- do.call("rbind", imps)
+                    imps <- aggregate(Overall ~ Variable,  data = imps, sum)
+                    imps$Overall <- imps$Overall/length(object$models)
+                    rownames(imps) <- as.character(imps$Variable)
+                    imps$Variable <- NULL
+                    imps
+                  },
+                  levels = function(x) x$obsLevels,
+                  tags = c("Ensemble Model", "Implicit Feature Selection", 
+                           "Feature Extraction Models", "Tree-Based Model"),
+                  sort = function(x) x[order(x[,1]),])
