1b107a70a86ea9298d98e57aa09cc0abd2e11164
topepo <mxkuhn@gmail.com>
2016-03-29 21:34:57 -0400

    bug fixes related to issue #396

--- models/files/glmboost.R
+++ models/files/glmboost.R
@@ -1,5 +1,5 @@
 modelInfo <- list(label = "Boosted Generalized Linear Model",
-                  library = "mboost",
+                  library = c("plyr", "mboost"),
                   type = c("Regression", "Classification"),
                   parameters = data.frame(parameter = c('mstop', 'prune'),
                                           class = c("numeric", "character"),
@@ -13,13 +13,16 @@ modelInfo <- list(label = "Boosted Generalized Linear Model",
                     }
                     out[!duplicated(out),]
                   },
-                  loop = function(grid) {   
-                    grid <- grid[order(grid$mstop, decreasing = TRUE),, drop = FALSE]
-                    loop <- grid[1,,drop = FALSE]
-                    submodels <- list(grid[-1, "mstop", drop = FALSE])         
-                    list(loop = loop, submodels = submodels)
+                  loop = function(grid) {
+                    grid <- grid[order(-grid$mstop, grid$prune),]
+                    loop <- ddply(grid, .(prune), function(x) data.frame(mstop = max(x$mstop)))
+                    submodels <- vector(mode = "list", length = nrow(loop))
+                    for(i in seq(along = loop$mstop)) {
+                      submodels[[i]] <- subset(grid, prune == loop$prune[i] & mstop < loop$mstop[i])
+                    }     
+                    list(loop = loop[, c("mstop", "prune")], submodels = submodels)
                   },
-                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {                
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {   
                     ##check for control list and over-write mstop
                     theDots <- list(...)
                     if(any(names(theDots) == "control"))
@@ -39,38 +42,55 @@ modelInfo <- list(label = "Boosted Generalized Linear Model",
                     dat$.outcome <- y
                     modelArgs <- c(list(formula = as.formula(".outcome ~ ."), data = dat, control = ctl), 
                                    theDots)
-                    
                     out <- do.call(mboost:::glmboost.formula, modelArgs)
                     
-                    if(param$prune == "yes")
-                    {
-                      out <- if(is.factor(y)) out[mstop(AIC(out, "classical"))] else out[mstop(AIC(out))]
+                    ## from `?mstop`: The [.mboost function can be used to enhance or restrict a given
+                    ## boosting model to the specified boosting iteration i. Note that in both cases the 
+                    ## original x will be changed to reduce the memory footprint. If the boosting model 
+                    ## is enhanced by specifying an index that is larger than the initial mstop, only 
+                    ## the missing i - mstop steps are fitted. If the model is restricted, the spare 
+                    ## steps are not dropped, i.e., if we increase i again, these boosting steps are 
+                    ## immediately available. Alternatively, the same operation can be done 
+                    ## by mstop(x) <- i.
+                    
+                    if(param$prune == "yes") {
+                      iters <- if(is.factor(y)) 
+                        mstop(AIC(out, "classical")) else 
+                          mstop(AIC(out))
+                      if(iters < out$mstop()) out <- out[iters] 
                     }
+                    out$.org.mstop <- out$mstop()
                     
                     ## for easier printing (and tracebacks), we'll try to make the calls shorter
-                    ## by adding dummy object names instead of the long obkect definitions that
+                    ## by adding dummy object names instead of the long object definitions that
                     ## currently exist
                     out$call["x"] <- "xData"         
                     out$call["y"] <- "yData"         
                     out
-                    },
+                  },
                   predict = function(modelFit, newdata, submodels = NULL) {
                     predType <- ifelse(modelFit$problemType == "Classification", "class", "response")
                     if(!is.data.frame(newdata)) newdata <- as.data.frame(newdata)
                     out <- predict(modelFit, newdata, type = predType)
-                    
-                    if(!is.null(submodels))
-                    {
+                    if(!is.null(submodels)) {
+                      
                       tmp <- vector(mode = "list", length = nrow(submodels) + 1)
                       tmp[[1]] <- as.vector(out)
-                      
-                      for(j in seq(along = submodels$mstop))
-                        tmp[[j+1]]  <- as.vector(predict(modelFit[submodels$mstop[j]], 
+                      for(j in seq(along = submodels$mstop)) {
+                        ## If the model has been pruned, make sure that the requested `mstop`
+                        ## is not greater than the original value. If it is, use the orignal value. 
+                        ## This should only occur whenm the model was pruned . 
+                        this_mstop <- if(submodels$prune[j] == "yes" & 
+                                         submodels$mstop[j] > modelFit$.org.mstop)
+                          modelFit$.org.mstop else submodels$mstop[j]
+                        tmp[[j+1]]  <- as.vector(predict(modelFit[this_mstop], 
                                                          newdata, 
                                                          type = predType))
-                      
+                      }
                       out <- tmp
+                      mstop(modelFit) <- modelFit$.org.mstop
                     } 
+                    # cat(modelFit$mstop(), "!\n")
                     out         
                   },
                   prob = function(modelFit, newdata, submodels = NULL) {
@@ -78,20 +98,22 @@ modelInfo <- list(label = "Boosted Generalized Linear Model",
                     lp <- predict(modelFit, newdata)
                     out <- cbind( binomial()$linkinv(-lp), 1 - binomial()$linkinv(-lp))
                     colnames(out) <- modelFit$obsLevels
-                    if(!is.null(submodels))
-                    {
+                    if(!is.null(submodels)) {
                       tmp <- vector(mode = "list", length = nrow(submodels) + 1)
                       tmp[[1]] <- out
-                      
-                      for(j in seq(along = submodels$mstop))
-                      {                           
-                        tmpProb <- predict(modelFit[submodels$mstop[j]], newdata)
+                      for(j in seq(along = submodels$mstop)) {    
+                        this_mstop <- if(submodels$prune[j] == "yes" & 
+                                         submodels$mstop[j] > modelFit$.org.mstop)
+                          modelFit$.org.mstop else submodels$mstop[j]
+                        
+                        tmpProb <- predict(modelFit[this_mstop], newdata)
                         tmpProb <- cbind(binomial()$linkinv(-tmpProb),
                                          1 - binomial()$linkinv(-tmpProb))
                         colnames(tmpProb) <- modelFit$obsLevels
                         tmp[[j+1]] <- as.data.frame(tmpProb[, modelFit$obsLevels,drop = FALSE])           
                       }
                       out <- tmp
+                      mstop(modelFit) <- modelFit$.org.mstop
                     }                        
                     out
                   },
