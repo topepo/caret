81b1bdb45004dae1b96cbf52aa67677f842ca302
topepo <mxkuhn@gmail.com>
2016-06-09 11:06:25 -0400

    fixed bug

--- RegressionTests/Code/mlpSGD.R
+++ RegressionTests/Code/mlpSGD.R
@@ -58,7 +58,7 @@ set.seed(849)
 test_reg_none_model <- train(trainX, trainY, 
                              method = "mlpSGD", 
                              trControl = rctrl3,
-                             tuneLength = 1,
+                             tuneGrid = test_reg_cv_model$bestTune,
                              preProc = c("center", "scale"))
 test_reg_none_pred <- predict(test_reg_none_model, testX)
 
