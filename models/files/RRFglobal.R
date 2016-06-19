47d50f2bd4e10dac83a159988bd8921cd8f1b0e6
topepo <mxkuhn@gmail.com>
2016-06-19 12:33:05 -0400

    bug fix for issue #442

--- models/files/RRFglobal.R
+++ models/files/RRFglobal.R
@@ -7,10 +7,10 @@ modelInfo <- list(label = "Regularized Random Forest",
                                           label = c('#Randomly Selected Predictors', 'Regularization Value')),
                   grid = function(x, y, len = NULL, search = "grid") {
                     if(search == "grid") {
-                      out <- data.frame(mtry = caret::var_seq(p = ncol(x), 
-                                                              classification = is.factor(y), 
-                                                              len = len),
-                                        coefReg = seq(0.01, 1, length = len))
+                      out <- expand.grid(mtry = caret::var_seq(p = ncol(x), 
+                                                               classification = is.factor(y), 
+                                                               len = len),
+                                         coefReg = seq(0.01, 1, length = len))
                     } else {
                       out <- data.frame(mtry = sample(1:ncol(x), size = len, replace = TRUE),
                                         coefReg = runif(len, min = 0, max = 1))
