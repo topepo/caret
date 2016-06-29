f3329a3d98459051e9ed7db503b71402076da153
terrytangyuan <terrytangyuan@gmail.com>
2016-06-28 21:24:33 -0400

    Updated updates.R and added tests

--- pkg/caret/R/updates.R
+++ pkg/caret/R/updates.R
@@ -1,6 +1,6 @@
 update.train <- function(object, param = NULL, forceRefit = FALSE, ...)
 {
-  if(forceRefit) param <- object$bestTune
+  if(forceRefit & is.null(param)) param <- object$bestTune
   if(is.null(param)) {
     if(all(names(object) != "modelInfo")) {
       funcs <- try(getModelInfo(object$method)[[1]], silent = TRUE)
