f970bd432b5594e7e0e3d7d27faf2d73b6efbc1f
Tyler Hunt <tyler.hunt@utah.edu>
2016-08-26 08:43:43 -0600

    optimizing filterVarImp

--- pkg/caret/R/filterVarImp.R
+++ pkg/caret/R/filterVarImp.R
@@ -1,133 +1,41 @@
-## todo start using foreach here
 
-oldfilterVarImp <- function(x, y, nonpara = FALSE, ...)
-{
-  {
-    notNumber <- unlist(lapply(x, function(x) !is.numeric(x)))
-    if(any(notNumber))
-      {
-        for(i in which(notNumber)) x[,i] <- as.numeric(x[,i])
-      }
-  }
-
-  if(is.factor(y))
-    {
-      classLevels <- levels(y)
-
-      outStat <- matrix(NA, nrow = dim(x)[2], ncol = length(classLevels))
-      for(i in seq(along = classLevels))
-        {
-          otherLevels <- classLevels[classLevels != classLevels[i]]
-
-          for(k in seq(along = otherLevels))
-            {
-              tmpSubset <- as.character(y) %in% c(classLevels[i], otherLevels[k])
-              tmpY <- factor(as.character(y)[tmpSubset])
-              tmpX <- x[tmpSubset,]
-
-              rocAuc <- apply(
-                              tmpX,
-                              2,
-                              function(x, class, pos)
-                              {
-                                isMissing <- is.na(x) | is.na(class)
-                                if(any(isMissing))
-                                  {
-                                    x <- x[!isMissing]
-                                    class <- class[!isMissing]
-                                  }
-                                outResults <- if(length(unique(x)) > 200) roc(x, class = class, positive = pos)
-                                else roc(x, class = class, dataGrid = FALSE, positive = pos)
-                                aucRoc(outResults)
-                              },
-                              class = tmpY,
-                              pos = classLevels[i])
-              outStat[, i] <- pmax(outStat[, i], rocAuc, na.rm = TRUE)
-            }
-          if(i ==1 & length(classLevels) == 2)
-            {
-              outStat[, 2] <- outStat[, 1]
-              break()
-            }
-        }
-      colnames(outStat) <- classLevels
-      rownames(outStat) <- dimnames(x)[[2]]
-      outStat <- data.frame(outStat)
-    } else {
-
-      paraFoo <- function(data, y) abs(coef(summary(lm(y ~ data, na.action = na.omit)))[2, "t value"])
-      nonparaFoo <- function(x, y, ...)
-        {
-          meanMod <- sum((y - mean(y, rm.na = TRUE))^2)
-          nzv <- nearZeroVar(x, saveMetrics = TRUE)
-
-          if(nzv$zeroVar) return(NA)
-          if(nzv$percentUnique < 20)
-            {
-              regMod <- lm(y~x, na.action = na.omit, ...)
-            } else {
-              regMod <- try(loess(y~x, na.action = na.omit, ...), silent = TRUE)
-
-              if(class(regMod) == "try-error" | any(is.nan(regMod$residuals))) try(regMod <- lm(y~x, ...))
-              if(class(regMod) == "try-error") return(NA)
-            }
-
-          pR2 <- 1 - (sum(resid(regMod)^2)/meanMod)
-          if(pR2 < 0) pR2 <- 0
-          pR2
-        }
-
-      testFunc <- if(nonpara) nonparaFoo else paraFoo
-
-      outStat <- apply(x, 2, testFunc, y = y)
-      outStat <- data.frame(Overall = outStat)
-    }
-  outStat
+rocPerCol <- function(dat, cls){
+  ModelMetrics::auc(cls, dat)
 }
 
-
-rocPerCol <- function(dat, cls) {
-  1 - ModelMetrics::auc(cls, dat)
+asNumeric <- function(data){
+  fc <- sapply(data, is.factor)
+  modifyList(data, lapply(data[, fc], as.numeric))
 }
 
-filterVarImp <- function(x, y, nonpara = FALSE, ...)
-{
-  {
-    notNumber <- sapply(x, function(x) !is.numeric(x))
-    if(any(notNumber))
-      {
-        for(i in which(notNumber)) x[,i] <- as.numeric(x[,i])
-      }
-  }
+filterVarImp <- function(x, y, nonpara = FALSE, ...){
+  # converting factors to numeric
+  notNumber <- sapply(x, function(x) !is.numeric(x))
+  x = asNumeric(x)
 
-  if(is.factor(y))
-    {
+  if(is.factor(y)){
       classLevels <- levels(y)
       k <- length(classLevels)
 
-      if(k > 2)
-        {
-          counter <- 1
-          classIndex <- vector(mode = "list", length = k)
-          tmpStat <- matrix(NA, nrow = ncol(x), ncol = choose(k, 2))
-          for(i in 1:k)
-            {
-              for(j in i:k)
-                {
-                  if(i != j)
-                    {
-                      classIndex[[i]] <- c(classIndex[[i]], counter)
-                      classIndex[[j]] <- c(classIndex[[j]], counter)
-                      index <- which(y %in% c(classLevels[i], classLevels[j]))
-                      tmpX <- x[index,,drop = FALSE]
-                      tmpY <- factor(as.character(y[index]), levels = c(classLevels[i], classLevels[j]))
-                      tmpStat[,counter] <- apply(tmpX, 2, rocPerCol, cls = tmpY)
-                      counter <- counter + 1
-                    }
-                }
-            }
-          outStat <- matrix(NA, ncol(x), k)
-          for(i in 1:k) outStat[,i] <- apply(tmpStat[,classIndex[[i]]], 1, max)
+      if(k > 2){
+
+        Combs <- combn(classLevels, 2)
+        CombsN <- combn(1:k, 2)
+
+          lStat <- lapply(1:ncol(Combs), FUN = function(cc){
+            yLevs <- as.character(y) %in% Combs[,cc]
+            tmpX <- x[yLevs,]
+            tmpY <- as.numeric(y[yLevs] == Combs[,cc][2])
+            apply(tmpX, 2, rocPerCol, cls = tmpY)
+          })
+          Stat = do.call("cbind", lStat)
+
+          loutStat <- lapply(1:k, function(j){
+            apply(Stat[,CombsN[,j]], 1, max)
+          })
+
+          outStat = do.call("cbind", loutStat)
+
         } else {
           tmp <- apply(x, 2, rocPerCol, cls = y)
           outStat <- cbind(tmp, tmp)
