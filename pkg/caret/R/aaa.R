c53de03ad21d1580e0f4ae0a04b9c7869fc39d9b
Max Kuhn <mxkuhn@gmail.com>
2016-10-25 16:00:55 -0400

    move away from :::
    * checking dependencies in R code ... NOTE
    ':::' call which should be '::': ‘ModelMetrics:::auc’
      See the note in ?`:::` about the use of this operator.

--- pkg/caret/R/aaa.R
+++ pkg/caret/R/aaa.R
@@ -241,7 +241,7 @@ twoClassSummary <- function (data, lev = NULL, model = NULL)
   if (!all(levels(data[, "pred"]) == lvls))
     stop("levels of observed and predicted data do not match")
   data$y = as.numeric(data$obs == lvls[2])
-  rocAUC <- ModelMetrics:::auc(ifelse(data$obs == lev[2], 0, 1), data[, lvls[1]])
+  rocAUC <- ModelMetrics::auc(ifelse(data$obs == lev[2], 0, 1), data[, lvls[1]])
   out <- c(rocAUC,
            sensitivity(data[, "pred"], data[, "obs"], lev[1]),
            specificity(data[, "pred"], data[, "obs"], lev[2]))
