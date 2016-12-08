e484d4f10be97d31ec24c93f4a818e9377b8fe1b
hadjipantelis <hadjipantelis@users.noreply.github.com>
2016-12-08 12:49:34 +0000

    Fix for issue #549

--- pkg/caret/R/createModel.R
+++ pkg/caret/R/createModel.R
@@ -56,6 +56,7 @@
     modelFit$problemType <- if(is.factor(y)) "Classification" else "Regression"
     modelFit$tuneValue <- tuneValue
     modelFit$obsLevels <- obsLevels
+    modelFit$params <- list(...)
   }
 
   list(fit = modelFit, preProc = ppObj)
