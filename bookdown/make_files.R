0196c06ac043994f65e5768c77aa46e2a5ae3fb4
topeop <mxkuhn@gmail.com>
2017-01-07 11:56:24 -0500

    enabled parallel processing using mc in all calculations

--- bookdown/make_files.R
+++ bookdown/make_files.R
@@ -1,5 +1,18 @@
+start_time <- proc.time()[3]
+
 library(bookdown)
+library(doMC)
+library(parallel)
+registerDoMC(cores = detectCores() - 1)
 
 render_book("index.Rmd", "bookdown::gitbook")
+
+stop_time <- proc.time()[3]
+
+cat(
+  "Execution took", 
+  round((stop_time-start_time)/60/60, 1),
+  "hours\n"
+)
 
 if(!interactive()) q("no")
