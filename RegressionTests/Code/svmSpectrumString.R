bd85fb9511723cdff05cc5329760443586b01d1f
Max Kuhn <mxkuhn@gmail.com>
2017-04-12 08:34:10 -0400

    Added a column name to the matrix

--- RegressionTests/Code/svmSpectrumString.R
+++ RegressionTests/Code/svmSpectrumString.R
@@ -7,6 +7,8 @@ model <- "svmSpectrumString"
 
 library(kernlab)
 data(reuters)
+reuters <- matrix(reuters, ncol = 1)
+colnames(reuters) <- "text"
 
 cctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all")
 cctrl2 <- trainControl(method = "LOOCV", savePredictions = TRUE)
@@ -15,32 +17,32 @@ cctrl3 <- trainControl(method = "none",
 cctrlR <- trainControl(method = "cv", number = 3, returnResamp = "all", search = "random")
 
 set.seed(849)
-test_class_cv_model <- train(matrix(reuters, ncol = 1), rlabels, 
+test_class_cv_model <- train(reuters, rlabels, 
                              method = "svmSpectrumString",
                              trControl = cctrl1)
 
-test_class_pred <- predict(test_class_cv_model, matrix(reuters, ncol = 1))
+test_class_pred <- predict(test_class_cv_model, reuters)
 
 set.seed(849)
-test_class_rand <- train(matrix(reuters, ncol = 1), rlabels, 
+test_class_rand <- train(reuters, rlabels, 
                          method = "svmSpectrumString", 
                          trControl = cctrlR,
                          tuneLength = 4)
 
 set.seed(849)
-test_class_loo_model <- train(matrix(reuters, ncol = 1), rlabels, 
+test_class_loo_model <- train(reuters, rlabels, 
                               method = "svmSpectrumString",
                               trControl = cctrl2)
 
 set.seed(849)
-test_class_none_model <- train(matrix(reuters, ncol = 1), rlabels, 
+test_class_none_model <- train(reuters, rlabels, 
                                method = "svmSpectrumString", 
                                trControl = cctrl3,
                                tuneGrid = test_class_cv_model$bestTune,
                                metric = "ROC")
 
-test_class_none_pred <- predict(test_class_none_model, matrix(reuters, ncol = 1))
-test_class_none_prob <- predict(test_class_none_model, matrix(reuters, ncol = 1), type = "prob")
+test_class_none_pred <- predict(test_class_none_model, reuters)
+test_class_none_prob <- predict(test_class_none_model, reuters, type = "prob")
 
 test_levels <- levels(test_class_cv_model)
 if(!all(levels(rlabels) %in% test_levels))
