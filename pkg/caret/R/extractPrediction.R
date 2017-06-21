67085b593d05fb710c13f7336eac2ab07054200a
Max Kuhn <mxkuhn@gmail.com>
2017-06-20 23:25:58 -0400

    Top-level truncation function used by workflows

--- pkg/caret/R/extractPrediction.R
+++ pkg/caret/R/extractPrediction.R
@@ -147,3 +147,38 @@ trimPredictions <- function(pred, mod_type, bounds, limits) {
   }
   pred
 }
+
+## This is used in workflows
+trim_values <- function(preds, ctrl, is_num) {
+  if(is_num) {
+    if(is.logical(ctrl$predictionBounds) && any(ctrl$predictionBounds)) {
+      if(is.list(preds)) {
+        preds <- lapply(preds, caret:::trimPredictions,
+                        mod_type = "Regression",
+                        bounds = ctrl$predictionBounds,
+                        limits = ctrl$yLimits)
+      } else {
+        preds <- caret:::trimPredictions(mod_type = "Regression",
+                                         bounds =  ctrl$predictionBounds,
+                                         limits =  ctrl$yLimit,
+                                         pred = preds)
+      }
+    } else {
+      if(is.numeric(ctrl$predictionBounds) && any(!is.na(ctrl$predictionBounds))) {
+        if(is.list(preds)) {
+          preds <- lapply(preds, caret:::trimPredictions,
+                          mod_type = "Regression",
+                          bounds = ctrl$predictionBounds,
+                          limits = ctrl$yLimits)
+        } else {
+          preds <- caret:::trimPredictions(mod_type = "Regression",
+                                           bounds =  ctrl$predictionBounds,
+                                           limits =  ctrl$yLimit,
+                                           pred = preds)
+        }
+      }
+    } 
+  }
+  preds
+}
+
