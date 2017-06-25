2101accaad058ae000453ed4a16a6e9851a623db
Max Kuhn <mxkuhn@gmail.com>
2017-06-25 16:29:05 -0400

    oblique.tree was removed

--- models/parseModels.R
+++ models/parseModels.R
@@ -1,6 +1,6 @@
 setwd("~/github/caret/models/files")
 
-exclusions <- c("^rknn", "[mM]xnet", "^sdda", "^enpls", "Boruta", "bdk")
+exclusions <- c("^rknn", "[mM]xnet", "^sdda", "^enpls", "Boruta", "bdk", "^oblique")
 exclusions <- paste0("(", exclusions, ")")
 exclusions <- paste0(exclusions, collapse = "|")
 
