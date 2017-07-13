dbccbdfeb401adcec3b394da42f4ae9bb9883853
Max Kuhn <mxkuhn@gmail.com>
2017-07-13 15:41:48 -0400

    Fixed method call

--- RegressionTests/Code/ordinalNet.R
+++ RegressionTests/Code/ordinalNet.R
@@ -14,6 +14,8 @@ testing <- twoClassSim(500, ordinal = TRUE)
 trainX <- training[, -ncol(training)]
 trainY <- training$Class
 
+wts <- runif(nrow(trainX))
+
 rec_cls <- recipe(Class ~ ., data = training) %>%
   step_center(all_predictors()) %>%
   step_scale(all_predictors())
@@ -36,14 +38,14 @@ cctrl5 <- trainControl(method = "LOOCV", summaryFunction = weight_test)
 
 set.seed(849)
 test_class_cv_model <- train(trainX, trainY, 
-                             method = "ordinalNet", 
+                             method ="ordinalNet", 
                              trControl = cctrl1,
                              metric = "Kappa", 
                              preProc = c("center", "scale"))
 
 set.seed(849)
 test_class_cv_form <- train(Class ~ ., data = training, 
-                            method = "ordinalNet", 
+                            method ="ordinalNet", 
                             trControl = cctrl1,
                             metric = "Kappa", 
                             preProc = c("center", "scale"))
@@ -55,7 +57,7 @@ test_class_prob_form <- predict(test_class_cv_form, testing[, -ncol(testing)], t
 
 set.seed(849)
 test_class_loo_model <- train(trainX, trainY, 
-                              method = "ordinalNet", 
+                              method ="ordinalNet", 
                               trControl = cctrl2,
                               metric = "Kappa", 
                               preProc = c("center", "scale"))
@@ -63,7 +65,7 @@ test_class_loo_model <- train(trainX, trainY,
 set.seed(849)
 
 test_class_none_model <- train(trainX, trainY, 
-                               method = "ordinalNet", 
+                               method ="ordinalNet", 
                                trControl = cctrl3,
                                tuneGrid = test_class_cv_model$bestTune,
                                metric = "Kappa", 
@@ -74,8 +76,8 @@ test_class_none_prob <- predict(test_class_none_model, testing[, -ncol(testing)]
 
 set.seed(849)
 test_class_cv_weight <- train(trainX, trainY, 
-                              weights = runif(nrow(trainX)),
-                              method = "ordinalNet", 
+                              weights = wts,
+                              method ="ordinalNet", 
                               trControl = cctrl4,
                               tuneLength = 1,
                               metric = "Accuracy", 
@@ -83,12 +85,70 @@ test_class_cv_weight <- train(trainX, trainY,
 
 set.seed(849)
 test_class_loo_weight <- train(trainX, trainY, 
-                               weights = runif(nrow(trainX)),
-                               method = "ordinalNet", 
+                               weights = wts,
+                               method ="ordinalNet", 
                                trControl = cctrl5,
                                tuneLength = 1,
                                metric = "Accuracy", 
                                preProc = c("center", "scale"))
+
+
+set.seed(849)
+test_class_cv_model <- train(trainX, trainY, 
+                             method ="ordinalNet", 
+                             trControl = cctrl1,
+                             metric = "Kappa", 
+                             preProc = c("center", "scale"))
+
+set.seed(849)
+test_class_cv_form_rec <- train(rec_cls, data = training, 
+                                method ="ordinalNet", 
+                                trControl = cctrl1,
+                                metric = "Kappa")
+
+if(
+  !isTRUE(
+    all.equal(test_class_cv_form$results, 
+              test_class_cv_form_rec$results))
+)
+  stop("CV weights not giving the same results")
+
+set.seed(849)
+test_class_loo_model_rec <- train(rec_cls, data = training, 
+                                  method ="ordinalNet", 
+                                  trControl = cctrl2,
+                                  metric = "Kappa")
+
+if(
+  !isTRUE(
+    all.equal(test_class_loo_model_rec$results, 
+              test_class_loo_model$results))
+)
+  stop("CV weights not giving the same results")
+
+
+tmp <- training
+tmp$wts <- wts
+
+weight_rec <- recipe(Class ~ ., data = tmp) %>%
+  add_role(wts, new_role = "case weight") %>%
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
+set.seed(849)
+test_class_cv_weight_rec <- train(weight_rec, data = tmp,
+                                  method ="ordinalNet", 
+                                  trControl = cctrl4,
+                                  tuneLength = 1,
+                                  metric = "Accuracy")
+
+if(
+  !isTRUE(
+    all.equal(test_class_cv_weight_rec$results, 
+              test_class_cv_weight$results))
+)
+  stop("CV weights not giving the same results")
+
 
 test_levels <- levels(test_class_cv_model)
 if(!all(levels(trainY) %in% test_levels))
