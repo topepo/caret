b440f39fbfceb8240899b503767575ec0e959bd9
Max Kuhn <mxkuhn@gmail.com>
2017-07-24 09:33:57 -0400

    Update for current release

--- libs/forceNetwork-binding-0.4/forceNetwork.js
+++ libs/forceNetwork-binding-0.4/forceNetwork.js
@@ -10,7 +10,7 @@ HTMLWidgets.widget({
         .attr("width", width)
         .attr("height", height);
 
-    return d3.layout.force();
+    return d3.forceSimulation();
   },
 
   resize: function(el, width, height, force) {
@@ -19,7 +19,8 @@ HTMLWidgets.widget({
         .attr("width", width)
         .attr("height", height);
 
-    force.size([width, height]).resume();
+    force.force("center", d3.forceCenter(width / 2, height / 2))
+        .restart();
   },
 
   renderValue: function(el, x, force) {
@@ -42,6 +43,17 @@ HTMLWidgets.widget({
     var links = HTMLWidgets.dataframeToD3(x.links);
     var nodes = HTMLWidgets.dataframeToD3(x.nodes);
 
+    // create linkedByIndex to quickly search for node neighbors
+    // adapted from: http://stackoverflow.com/a/8780277/4389763
+    var linkedByIndex = {};
+    links.forEach(function(d) {
+      linkedByIndex[d.source + "," + d.target] = 1;
+      linkedByIndex[d.target + "," + d.source] = 1;
+    });
+    function neighboring(a, b) {
+      return linkedByIndex[a.index + "," + b.index];
+    }
+
     // get the width and height
     var width = el.offsetWidth;
     var height = el.offsetHeight;
@@ -49,26 +61,35 @@ HTMLWidgets.widget({
     var color = eval(options.colourScale);
 
     // set this up even if zoom = F
-    var zoom = d3.behavior.zoom();
+    var zoom = d3.zoom();
 
     // create d3 force layout
     force
       .nodes(d3.values(nodes))
-      .links(links)
-      .size([width, height])
-      .linkDistance(options.linkDistance)
-      .charge(options.charge)
-      .on("tick", tick)
-      .start();
+      .force("link", d3.forceLink(links).distance(options.linkDistance))
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .force("charge", d3.forceManyBody().strength(options.charge))
+      .on("tick", tick);
 
-    // thanks http://plnkr.co/edit/cxLlvIlmo1Y6vJyPs6N9?p=preview
-    //  http://stackoverflow.com/questions/22924253/adding-pan-zoom-to-d3js-force-directed
-      var drag = force.drag()
-        .on("dragstart", dragstart)
-      // allow force drag to work with pan/zoom drag
+    force.alpha(1).restart();
+
+      var drag = d3.drag()
+        .on("start", dragstart)
+        .on("drag", dragged)
+        .on("end", dragended)
       function dragstart(d) {
-        d3.event.sourceEvent.preventDefault();
-        d3.event.sourceEvent.stopPropagation();
+        if (!d3.event.active) force.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+      function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+      }
+      function dragended(d) {
+        if (!d3.event.active) force.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
       }
 
     // select the svg element and remove existing children
@@ -83,9 +104,8 @@ HTMLWidgets.widget({
     // add zooming if requested
     if (options.zoom) {
       function redraw() {
-        d3.select(el).select(".zoom-layer").attr("transform",
-          "translate(" + d3.event.translate + ")"+
-          " scale(" + d3.event.scale + ")");
+        d3.select(el).select(".zoom-layer")
+          .attr("transform", d3.event.transform);
       }
       zoom.on("zoom", redraw)
 
@@ -99,7 +119,7 @@ HTMLWidgets.widget({
 
     // draw links
     var link = svg.selectAll(".link")
-      .data(force.links())
+      .data(links)
       .enter().append("line")
       .attr("class", "link")
       .style("stroke", function(d) { return d.colour ; })
@@ -115,6 +135,27 @@ HTMLWidgets.widget({
             .style("opacity", options.opacity);
       });
 
+    if (options.arrows) {
+      link.style("marker-end",  function(d) { return "url(#arrow-" + d.colour + ")"; });
+
+      var linkColoursArr = d3.nest().key(function(d) { return d.colour; }).entries(links);
+
+      svg.append("defs").selectAll("marker")
+          .data(linkColoursArr)
+          .enter().append("marker")
+            .attr("id", function(d) { return "arrow-" + d.key; })
+            .attr("viewBox", "0, -5, 10, 10")
+            .attr("refX", 0)
+            .attr("markerWidth", 4)
+            .attr("markerHeight", 4)
+            .attr("orient", "auto")
+            .style("fill", "context-fill")
+            .style("fill", function(d) { return d.key; })
+            .style("opacity", options.opacity)
+          .append("path")
+            .attr("d", "M0,-5 L10,0 L0,5");
+    }
+
     // draw nodes
     var node = svg.selectAll(".node")
       .data(force.nodes())
@@ -125,7 +166,7 @@ HTMLWidgets.widget({
       .on("mouseover", mouseover)
       .on("mouseout", mouseout)
       .on("click", click)
-      .call(force.drag);
+      .call(drag);
 
     node.append("circle")
       .attr("r", function(d){return nodeSize(d);})
@@ -148,17 +189,44 @@ HTMLWidgets.widget({
             d.x = Math.max(nodeSize(d), Math.min(width - nodeSize(d), d.x));
             d.y = Math.max(nodeSize(d), Math.min(height - nodeSize(d), d.y));
         }
-        
+
         return "translate(" + d.x + "," + d.y + ")"});
-        
+
+      function idx(d, type) {
+        var linkWidthFunc = eval("(" + options.linkWidth + ")");
+			  var a = d.target.x - d.source.x;
+			  var b = d.target.y - d.source.y;
+			  var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+  			if (type == "x1") return (d.source.x + ((nodeSize(d.source) * a) / c));
+  			if (type == "y1") return (d.source.y + ((nodeSize(d.source) * b) / c));
+  			if (options.arrows) {
+  			  if (type == "x2") return (d.target.x - ((((5 * linkWidthFunc(d)) + nodeSize(d.target)) * a) / c));
+  			  if (type == "y2") return (d.target.y - ((((5 * linkWidthFunc(d)) + nodeSize(d.target)) * b) / c));
+  			} else {
+  			  if (type == "x2") return (d.target.x - ((nodeSize(d.target) * a) / c));
+  			  if (type == "y2") return (d.target.y - ((nodeSize(d.target) * b) / c));
+  			}
+		  }
+
       link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("x1", function(d) { return idx(d, "x1"); })
+        .attr("y1", function(d) { return idx(d, "y1"); })
+        .attr("x2", function(d) { return idx(d, "x2"); })
+        .attr("y2", function(d) { return idx(d, "y2"); });
     }
 
-    function mouseover() {
+    function mouseover(d) {
+      // unfocus non-connected links and nodes
+      //if (options.focusOnHover) {
+        var unfocusDivisor = 4;
+
+        link.transition().duration(200)
+          .style("opacity", function(l) { return d != l.source && d != l.target ? +options.opacity / unfocusDivisor : +options.opacity });
+
+        node.transition().duration(200)
+          .style("opacity", function(o) { return d.index == o.index || neighboring(d, o) ? +options.opacity : +options.opacity / unfocusDivisor; });
+      //}
+
       d3.select(this).select("circle").transition()
         .duration(750)
         .attr("r", function(d){return nodeSize(d)+5;});
@@ -171,16 +239,19 @@ HTMLWidgets.widget({
     }
 
     function mouseout() {
+      node.style("opacity", +options.opacity);
+      link.style("opacity", +options.opacity);
+
       d3.select(this).select("circle").transition()
         .duration(750)
         .attr("r", function(d){return nodeSize(d);});
       d3.select(this).select("text").transition()
         .duration(1250)
         .attr("x", 0)
-        .style("font", options.fontSize + "px ") 
+        .style("font", options.fontSize + "px ")
         .style("opacity", options.opacityNoHover);
     }
-    
+
     function click(d) {
       return eval(options.clickAction)
     }
@@ -213,7 +284,7 @@ HTMLWidgets.widget({
           .attr('y', legendRectSize - legendSpacing)
           .text(function(d) { return d; });
     }
-    
+
     // make font-family consistent across all elements
     d3.select(el).selectAll('text').style('font-family', options.fontFamily);
   },
