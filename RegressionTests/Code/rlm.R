b8eb7bcc880faa8bf6a7830818a8805ad9d619bf
Max Kuhn <mxkuhn@gmail.com>
2017-11-09 09:52:56 -0500

    Removed the code that loads the package namespace.
    This code helped with reproducibility but obscured some errors related
    to loading the namespaces in the model code.

--- RegressionTests/Code/rlm.R
+++ RegressionTests/Code/rlm.R
@@ -6,11 +6,7 @@ library(dplyr)
 
 model <- "rlm"
 
-## In case the package or one of its dependencies uses random numbers
-## on startup so we'll pre-load the required libraries: 
 
-for(i in getModelInfo(model)[[1]]$library)
-  do.call("requireNamespace", list(package = i))
 
 #########################################################################
 
