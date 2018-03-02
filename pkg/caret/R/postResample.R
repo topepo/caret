ebbec66203cfa8809edaeb4a0bb1c7e840e9a793
djacobs7 <djacobs7@gmail.com>
2018-03-02 09:58:01 -0800

    Fix Error handling in postResample
    PostResample previously threw a cryptic error message if the type of the observed variable is logical.   
    This clarifies the error message.
    
    This blog post mentions the problem, which apparently has affected several people: http://davidhughjones.blogspot.com/2015/04/r-tip-caret-error.html
    
    The previous error message is below
    
    Error in `[.default`(tab, 1:m, 1:m) : subscript out of bounds
    13.
    NextMethod()
    12.
    `[.table`(tab, 1:m, 1:m)
    11.
    tab[1:m, 1:m]
    10.
    diag(tab[1:m, 1:m])
    9.
    e1071::classAgreement(table(obs, pred))
    8.
    unlist(e1071::classAgreement(table(obs, pred)))
    7.
    postResample(data[, "pred"], data[, "obs"])
    6.
    ctrl$summaryFunction(testOutput, lev, method)
    5.
    evalSummaryFunction(y, wts = weights, ctrl = trControl, lev = classLevels, metric = metric, method = method)
    4.
    train.default(x, y, weights = w, ...)
    3.
    train(x, y, weights = w, ...)
    2.
    train.formula(as.formula(formula), data = watts_swingy_rings_and_keynodes, method = "rpart", trControl = tc, metric = "Kappa")
    1.
    caret::train(as.formula(formula), data = watts_swingy_rings_and_keynodes, method = "rpart", trControl = tc, metric = "Kappa")

--- pkg/caret/R/postResample.R
+++ pkg/caret/R/postResample.R
@@ -143,6 +143,10 @@ postResample <- function(pred, obs)
         }
       names(out) <- c("RMSE", "Rsquared", "MAE")
     } else {
+    
+      if ( is.logical( obs ) ){
+        stop( "logical type output variables are not presently supported.  Try converting your output variable to a factor". )
+      }
       if(length(obs) + length(pred) == 0)
         {
           out <- rep(NA, 2)
