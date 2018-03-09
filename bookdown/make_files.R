24b54c817dea4b21996c3d8112e3310486c2d447
Max Kuhn <mxkuhn@gmail.com>
2018-03-09 11:22:44 -0500

    typeo fix and move (back) to forking

--- bookdown/make_files.R
+++ bookdown/make_files.R
@@ -1,7 +1,7 @@
 library(bookdown)
 library(parallel)
 library(doParallel)
-cl <- makePSOCKcluster(parallel::detectCores(logical = TRUE))
+cl <- makeForkCluster(parallel::detectCores(logical = TRUE))
 registerDoParallel(cl)
 
 render_book("index.Rmd", "bookdown::gitbook")
