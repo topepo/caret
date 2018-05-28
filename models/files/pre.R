9aed85e8f15b2a5828d5a2c340f4ab82e8cde2e2
Marjolein Fokkema <marjolein.fokkema@gmail.com>
2018-05-29 00:55:12 +0200

    Update pre.R

--- models/files/pre.R
+++ models/files/pre.R
@@ -43,6 +43,19 @@ modelInfo <- list(
   },
   fit = function(x, y, wts = NULL, param, lev = NULL, last = NULL, 
                  weights = NULL, classProbs, ...) { 
+    
+    theDots <- list(...)
+    if(!any(names(theDots) == "family")) {
+     theDots$family <- if (is.factor(y)) {
+       if (nlevels(y) == 2L) { 
+         "binomial"
+       } else {
+         "multinomial"
+       }
+     } else {
+       "gaussian"
+    }
+      }
     data <- data.frame(x, .outcome = y)
     formula <- .outcome ~ .
     if (is.null(weights)) { weights <- rep(1, times = nrow(x)) }
