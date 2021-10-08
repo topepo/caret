518571a26d9be699afe98d69f3173692f292fb36
Max Kuhn <mxkuhn@gmail.com>
2021-10-08 10:52:23 -0400

    remove srcref for smaller file size

--- models/parseModels.R
+++ models/parseModels.R
@@ -7,9 +7,12 @@ names(models) <- gsub("\\.R$", "", modelFiles)
 
 for(i in seq(along = modelFiles)) {
   source(modelFiles[i])
+  modelInfo <- lapply(modelInfo, rlang::zap_srcref)
   models[[i]] <- modelInfo
   rm(modelInfo)
 }
+
+
 
 save(models, file = "../../pkg/caret/inst/models/models.RData", version = 2)
 
