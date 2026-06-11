58d66015f1fd2d1fc36b94b615e86083d4c49617
Charlotte Zhang <chjxzh256@gmail.com>
2026-06-11 13:07:18 -0400

    Move RegressionTests, models/deprecated, html, deprecated, docs to caret-archive (#1419)
    Archived to topepo/caret-archive with full history preserved (312 commits).

--- deprecated/generateExprVal.method.trimMean.R
+++
@@ -1,7 +0,0 @@
-"generateExprVal.method.trimMean" <-
-function(probes, trim = 0.15) 
-{
-   mu <- apply(probes, 2, mean, trim = trim)
-   s <- rep(NA, length(mu))
-   return(list(exprs = mu, se.exprs = s))
-}
